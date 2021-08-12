de680ee1f343455efce347c839c8a7a78b50b248
Daniel Calvin <arockne2003@yahoo.com>
2021-08-12 09:37:35 -0700

    Get categories

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //when the page loads append categories to categories container
 //add an event listener when the category is clicked a dropdown of subcategories for that category will load
+function getCategories() {
+  fetch('https://www.dnd5eapi.co/api/')
+  .then(resp => resp.json())
+  .then(categories => console.log(categories))
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+  getCategories();
 })
