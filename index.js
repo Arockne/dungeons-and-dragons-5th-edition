d07cddb6f359dd07d684e31c22611270c8e03fe1
Daniel Calvin <arockne2003@yahoo.com>
2021-08-12 09:27:20 -0700

    Add event listener when page loads

---
+++ index.js
@@ -0,0 +1,6 @@
+//when the page loads append categories to categories container
+//add an event listener when the category is clicked a dropdown of subcategories for that category will load
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+})
