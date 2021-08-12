e7218fc168cc5874d3693acd3703c60c7f96aeb8
Daniel Calvin <arockne2003@yahoo.com>
2021-08-12 14:00:23 -0700

    Implement getters to get items out of each set

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 //when the page loads append categories to categories container
 //add an event listener when the category is clicked a dropdown of subcategories for that category will load
-function getCategories() {
+function getHeadings() {
   fetch('https://www.dnd5eapi.co/api/')
   .then(resp => {
     if (!resp.ok) throw resp
     return resp.json();
   })
-  .then(handleCategories)
+  .then(handleHeadings)
   .catch(err => {
     const message = `Error type: (${err.type}) Fetch from: (${err.url}) Status: (${err.status})`
     document.querySelector('body').textContent = message;
   })
 }
 
-function getSubCategory(category) {
+function getSet(category) {
   fetch(`https://www.dnd5eapi.co/api/${category}`)
   .then(resp => {
     if (!resp.ok) throw resp
     return resp.json();
   })
-  .then(sub => console.log(sub))
+  .then(set => set.results.forEach(getItem))
   .catch(err => {
     const message = `Error type: (${err.type}) Fetch from: (${err.url}) Status: (${err.status})`
     document.querySelector('body').textContent = message;
   })
 }
 
-function handleCategories(categories) {
+function getItem(set) {
+  fetch(`https://www.dnd5eapi.co${set.url}`)
+  .then(resp => resp.json())
+  .then(item => console.log(item))
+  .catch(err => {
+    const message = `Error type: (${err.type}) Fetch from: (${err.url}) Status: (${err.status})`
+    document.querySelector('body').textContent = message;
+  })
+}
+
+function handleHeadings(categories) {
   const categoriesArr = Object.keys(categories);
   categoriesArr.forEach(category => {
     const li = document.createElement('li');
     li.textContent = category.replace(/-/, ' ');
     li.className = category;
-    li.addEventListener('click', () => getSubCategory(category))
+    li.addEventListener('click', () => getSet(category))
     //posibly add span for an arrow when li element is clicked it will point down when li element is clicked and point left when subcategories are not shoing
     document.querySelector('#categories-container').appendChild(li);
   })
@@ -42,7 +52,7 @@ function handleCategories(categories) {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  getCategories();
+  getHeadings();
 })
 
 //REQUIREMENTS
