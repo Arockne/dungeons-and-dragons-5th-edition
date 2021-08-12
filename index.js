4d33a5c8f312780e946871824a6526366e00428a
Daniel Calvin <arockne2003@yahoo.com>
2021-08-12 10:58:51 -0700

    Implement handleCategories to append list elements to categories container

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 //when the page loads append categories to categories container
 //add an event listener when the category is clicked a dropdown of subcategories for that category will load
 function getCategories() {
-  fetch('https://www.dnd5eapi.co/ai/')
+  fetch('https://www.dnd5eapi.co/api/')
   .then(resp => {
-    console.log(resp)
     if (!resp.ok) throw resp
     return resp.json();
   })
-  .then(categories => console.log(categories))
+  .then(handleCategories)
   .catch(err => {
     const message = `Error type: (${err.type}) Fetch from: (${err.url}) Status: (${err.status})`
     document.querySelector('body').textContent = message;
   })
 }
 
+function handleCategories(categories) {
+  //convert object to an array of arrays
+  //for each category
+    //create a list element
+      //label the li the current category
+  //append the list element to the categories-container
+  const categoriesArray = Object.entries(categories);
+  categoriesArray.forEach(category => {
+    const li = document.createElement('li');
+    li.textContent = category[0].replace(/-/, ' ');
+    li.className = category[0];
+    document.querySelector('#categories-container').appendChild(li);
+  })
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   getCategories();
